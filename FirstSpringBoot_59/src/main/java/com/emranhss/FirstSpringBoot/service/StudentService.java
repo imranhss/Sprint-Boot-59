2fce1db281c6e2a9ed9756c390703e47ff98e3e7
Muhammad Emran Hossain <emranhss@gmail.com>
2024-08-22 17:45:25 +0600

    Add

---
+++ FirstSpringBoot_59/src/main/java/com/emranhss/FirstSpringBoot/service/StudentService.java
@@ -0,0 +1,46 @@
+package com.emranhss.FirstSpringBoot.service;
+
+import com.emranhss.FirstSpringBoot.entity.Student;
+import com.emranhss.FirstSpringBoot.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+@Service
+public class StudentService{
+
+    @Autowired
+    private  StudentRepository studentRepository;
+
+    public  void saveStu(Student s){
+        studentRepository.save(s);
+    }
+
+    public  List<Student>  getAllStu(){
+
+         return studentRepository.findAll();
+    }
+
+    public  void deleteById(int id){
+        studentRepository.deleteById(id);
+    }
+
+    public  Student findById(int id){
+
+        return  studentRepository.findById(id).get();
+    }
+
+    public  void updateStudent(Student s, int id){
+        studentRepository.save(s);
+
+    }
+
+}
